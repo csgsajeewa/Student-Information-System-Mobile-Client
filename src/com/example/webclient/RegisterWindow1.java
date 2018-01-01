47f03b65fe64d2b98597869b5a4adbd02e3fa36e
chamaths <chamaths@lseg.com>
2018-01-01 11:58:02 +0530

    feature change c 3

--- src/com/example/webclient/RegisterWindow1.java
+++ src/com/example/webclient/RegisterWindow1.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.widget.RadioButton;
 
 import android.widget.Toast;
-
+sfasdfasdfs
 public class RegisterWindow1 extends Activity implements AsyncResponse{
    
 	private RegisterTask registerTask;
